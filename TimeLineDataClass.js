940a85dd465eb2679f36b7fb03ce6acc2aab874b
dyz <dyzisaboy@gmail.com>
2020-10-04 14:21:50 +0800

    improve class definition: constructor & method.

--- TimeLineDataClass.js
+++ TimeLineDataClass.js
@@ -5,7 +5,7 @@
 class SegmentData {
 
     /**
-     * 
+     * @constructor Constructor of SegmentData.
      * @param {Date} startDate start date of timeRange
      * @param {Date} endDate end date of timeRange
      * @param {*} value value for property-val, could be integer or string or object
@@ -23,37 +23,37 @@ class SegmentData {
  * @description the class of TimeLine Data - GroupData, contains an array of SegmentData.
  */
 class GroupData {
+
+    /**
+     * @property Label of GroupData.
+     */
     label = "";
+
+    /**
+     * @property data array.
+     */
     data = [];
 
     /**
-     * 
-     * @param {String} label Label of GroupData
+     * @constructor Constructor of GroupData.
+     * @param {String} label GroupData Label.
+     * @param {SegmentData[]} data SegmentData Array.
      */
-    constructor(label = "unnamed label") {
-        // this(label, null);
+    constructor(label = "unnamed label", data) {
         this.label = label;
+        if (data === undefined) {
+            this.addSegmentData(data);
+        }
     }
-
-    // constructor(label = "unnamed label", data) {
-    //     this.label = label;
-    //     if (typeof(data) === Array) {
-    //         data.forEach(segment => {
-    //             this.addSegmentData(segment);
-    //         });
-    //     } else {
-    //         this.addSegmentData(data);
-    //     }
-    // }
 
     /**
      * @description add one SegmentData into this GroupData.
-     * @param {SegmentData} segmentData the segment for add
+     * @param {...SegmentData} segmentData the segment(s) for adding
      */
-    addSegmentData(segmentData) {
-        if (Object.getPrototypeOf(segmentData) === SegmentData.prototype) {
-            this.data.push(segmentData);
-        }
+    addSegmentData(...segmentData) {
+        segmentData = segmentData.filter(e => e !== undefined); // remove undefined elements.
+        segmentData = segmentData.filter(e => Object.getPrototypeOf(e) === SegmentData.prototype); // remove invalid elements.
+        segmentData.forEach(e => this.data.push(e)); // add all segmentData.
     }
 }
 
@@ -62,28 +62,36 @@ class GroupData {
  * @description the class of TimeLine data - Group, contains an array of GroupData.
  */
 class Group {
+
+    /**
+     * @property Group name.
+     */
     group = "";
+
+    /**
+     * @property data array.
+     */
     data = [];
 
-    constructor(group = "unnamed label") {
-        // this(group, null);
+    /**
+     * @constructor Constructor of GroupData.
+     * @param {String} group Group name.
+     * @param {GroupData[]} data GroupData Array.
+     */
+    constructor(group = "unnamed group", data) {
         this.group = group;
+        if (data === undefined) {
+            this.addGroupData(data);
+        }
     }
 
-    // constructor(group = "unnamed group", data) {
-    //     this.group = group;
-    //     if (typeof(data) === Array) {
-    //         data.forEach(groupData => {
-    //             this.addGroupData(groupData);
-    //         });
-    //     } else {
-    //         this.addGroupData(data);
-    //     }
-    // }
-
-    addGroupData(groupData) {
-        if (Object.getPrototypeOf(groupData) === GroupData.prototype) {
-            this.data.push(groupData);
-        }
+    /**
+     * @description add one GroupData into this Group.
+     * @param  {...GroupData} groupData the groupData(s) for adding
+     */
+    addGroupData(...groupData) {
+        groupData = groupData.filter(e => e !== undefined); // remove undefined elements.
+        groupData = groupData.filter(e => Object.getPrototypeOf(e) === GroupData.prototype); // remove invalid elements.
+        groupData.forEach(e => this.data.push(e)); // add all segmentData.
     }
 }
